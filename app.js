2d1550fa990bbf765c4f09844063b12f32a8a2ee
Noelia Perdomo <noelia.perdomo88@gmail.com>
2025-07-04 20:49:59 -0300

    menu hamburguesa

---
+++ app.js
@@ -0,0 +1,18 @@
+var catalogo = [];
+var carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+var filtro = null;
+var page = 1;
+
+//Elementos del DOM
+
+const listElegi_pasta = document.querySelector("#elegi_pasta > ul");
+const listCart = document.querySelector("#cart > ul");
+
+
+const btnMenu = document.getElementById("btnMenu");
+const navbar = document.getElementById("navbar");
+
+btnMenu.addEventListener("click", (e) => {
+  e.preventDefault();
+  navbar.classList.toggle("active");
+});
