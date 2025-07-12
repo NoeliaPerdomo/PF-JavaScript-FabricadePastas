b9228e50d8ab6737d1c6a13e717a4b58182678e4
Noelia Perdomo <noelia.perdomo88@gmail.com>
2025-07-11 21:31:15 -0300

    carrito

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ var carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 var filtro = null;
 var page = 1;
 
-//Elementos del DOM
-
+// Elementos del DOM
 const listElegi_pasta = document.querySelector("#elegi_pasta > ul");
 const listCart = document.querySelector("#cart > ul");
-
 
 const btnMenu = document.getElementById("btnMenu");
 const navbar = document.getElementById("navbar");
@@ -15,5 +13,14 @@ const navbar = document.getElementById("navbar");
 btnMenu.addEventListener("click", (e) => {
   e.preventDefault();
   navbar.classList.toggle("active");
+});
+
+// Mostrar/Ocultar carrito
+const btnCarrito = document.getElementById("btnCarrito");
+const cart = document.getElementById("cart");
+
+btnCarrito.addEventListener("click", (e) => {
+  e.preventDefault();
+  cart.classList.toggle("active");
 });
 
